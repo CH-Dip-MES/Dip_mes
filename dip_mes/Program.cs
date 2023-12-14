5455a8f2d8fc2e94ae7ae71763206fb2cb078316
swsong92 <153571375+swsong92@users.noreply.github.com>
2023-12-14 18:04:09 +0900

    Update Program.cs
    Program.cs
    dip_mes.login_screen -> main_screen

--- dip_mes/Program.cs
+++ dip_mes/Program.cs
@@ -16,8 +16,7 @@ namespace dip_mes
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new dip_mes.join_screen());
-
+            Application.Run(new main_screen());
         }
     }
 }
