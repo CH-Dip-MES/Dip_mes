67da3ff8316727a9a8f31ebc08a8c84759f0af93
건우 박 <pgw1729@gmail.com>
2023-12-14 10:23:05 +0900

    base_screen -> main_screen

---
+++ dip_mes/main_screen.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dip_mes
+{
+    public partial class base_screen : Form
+    {
+        buy.buy01 buySc1 = new buy.buy01();
+        sale.sale01 saleSc1 = new sale.sale01();
+        product.product02 productSc2 = new product.product02();
+        public base_screen()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form_load(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel.Controls.Clear();
+            panel.Controls.Add(buySc1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel.Controls.Clear();
+            panel.Controls.Add(saleSc1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panel.Controls.Clear();
+            panel.Controls.Add(productSc2);
+        }
+    }
+}
