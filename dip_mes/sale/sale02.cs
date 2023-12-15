27623d872c8b38454bf0eded81ddd5b894d48332
건우 박 <pgw1729@gmail.com>
2023-12-15 14:14:37 +0900

    Req6-1,2,6,7 완료 3,4,5 구현중

--- dip_mes/sale/sale02.cs
+++ dip_mes/sale/sale02.cs
@@ -28,7 +28,7 @@ namespace dip_mes.sale
                 using (MySqlConnection iConn = new MySqlConnection(jConn))
                 {
                     iConn.Open();
-                    MySqlCommand msc = new MySqlCommand("insert into sale2(saledate, salecode, buyername) values(@saledate, @salecode, @buyername)", iConn);
+                    MySqlCommand msc = new MySqlCommand("insert into sale2(saledate, salecode, buyername, delidate) values(@saledate, @salecode, @buyername)", iConn);
                     msc.Parameters.AddWithValue("@saledate", saledate.Value);
                     msc.Parameters.AddWithValue("@salecode", salecode.Text);
                     msc.Parameters.AddWithValue("@buyername", buyername.SelectedItem.ToString());
@@ -48,7 +48,8 @@ namespace dip_mes.sale
             {
                 sConn.Open();
                 string fSellNo = findNo.Text.Trim(); // 검색창 텍스트
-                string sInfo = "select * from sale2";
+                string sInfo = "select saledate,salecode,buyername,procstat,delidate from sale2";
+
                 if (!string.IsNullOrEmpty(fSellNo)) // 검색창에 입력한 문자 있을 시 활성화 없으면 위의 fItem 문구 그대로
                 {
                     sInfo += $" WHERE salecode = '{fSellNo}'";
@@ -64,24 +65,50 @@ namespace dip_mes.sale
                         // Check if any rows are returned
                         if (sManage.Rows.Count > 0)
                         {
-                            DataGridViewComboBoxColumn comboColumn = new DataGridViewComboBoxColumn();
-                            comboColumn.DataPropertyName = "procstat";
-                            comboColumn.HeaderText = "상태";
-                            comboColumn.Name = "ItemStat";
-                            comboColumn.Items.Add("입고");
-                            comboColumn.Items.Add("출고");
-
                             // DataGridView에 데이터 설정
                             dataGridView1.DataSource = sManage;
-
+                        
                             // DataGridView 컬럼 헤더 텍스트 설정
                             dataGridView1.Columns["saledate"].HeaderText = "등록일자";
                             dataGridView1.Columns["salecode"].HeaderText = "판매번호";
                             dataGridView1.Columns["buyername"].HeaderText = "고객명";
-                            dataGridView1.Columns["procstat"].HeaderText = "진행상태";
-                            dataGridView1.Columns["delistat"].HeaderText = "납품상태";
                             dataGridView1.Columns["delidate"].HeaderText = "납품일자";
-                        }                      
+
+                            // 기존의 "진행상태" 열을 제거
+                            if (dataGridView1.Columns.Contains("procstat"))
+                                dataGridView1.Columns.Remove("procstat");
+
+                            // 기존의 "납품상태" 열을 제거
+                            if (dataGridView1.Columns.Contains("delistat"))
+                                dataGridView1.Columns.Remove("delistat");
+
+                            // "진행상태" 콤보박스 열 추가
+                            DataGridViewComboBoxColumn comboColumnProcStat = new DataGridViewComboBoxColumn();
+                            comboColumnProcStat.DataPropertyName = "procstat";
+                            comboColumnProcStat.HeaderText = "진행상태";
+                            comboColumnProcStat.Name = "procstat";
+                            comboColumnProcStat.Items.Add("미진행");
+                            comboColumnProcStat.Items.Add("진행중");
+                            comboColumnProcStat.Items.Add("완료");
+
+                            // "진행상태" 콤보박스 열을 원래 "진행상태" 열의 인덱스에 추가
+
+                            dataGridView1.Columns.Insert(3, comboColumnProcStat);
+
+                            // "납품상태" 콤보박스 열 추가
+                            DataGridViewComboBoxColumn comboColumnDeliStat = new DataGridViewComboBoxColumn();
+                            comboColumnDeliStat.DataPropertyName = "delistat";
+                            comboColumnDeliStat.HeaderText = "납품상태";
+                            comboColumnDeliStat.Name = "delistat";
+                            comboColumnDeliStat.Items.Add("미납품");
+                            comboColumnDeliStat.Items.Add("배송중");
+                            comboColumnDeliStat.Items.Add("완료");
+
+                            dataGridView1.Columns.Insert(4, comboColumnDeliStat);
+
+                            dataGridView1.CellValueChanged += DataGridView1_CellValueChanged;
+
+                        }
                         else
                         {
                             MessageBox.Show("해당 품번의 데이터가 없습니다.");
@@ -101,20 +128,79 @@ namespace dip_mes.sale
                 DataGridView dataGridView = (DataGridView)sender;
                 DataGridViewCell changedCell = dataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex];
 
-                if (changedCell is DataGridViewComboBoxCell comboCell)
+                if (dataGridView.Columns[e.ColumnIndex].Name == "procstat" || dataGridView.Columns[e.ColumnIndex].Name == "delistat")
                 {
-                    // 추가로직: 콤보박스의 값이 "완료"일 때 Inven 열에 현재 날짜를 저장
-                    if (comboCell.Value != null && comboCell.Value.ToString() == "완료")
+                    bool isProcStatComplete = dataGridView.Rows[e.RowIndex].Cells["procstat"].Value != null && dataGridView.Rows[e.RowIndex].Cells["procstat"].Value.ToString() == "완료";
+                    bool isDeliStatComplete = dataGridView.Rows[e.RowIndex].Cells["delistat"].Value != null && dataGridView.Rows[e.RowIndex].Cells["delistat"].Value.ToString() == "완료";
+
+                    // 이전 값과 현재 값 가져오기
+                    string previousProcStat = dataGridView.Rows[e.RowIndex].Cells["procstat"].Tag?.ToString();
+                    string previousDeliStat = dataGridView.Rows[e.RowIndex].Cells["delistat"].Tag?.ToString();
+                    string currentProcStat = dataGridView.Rows[e.RowIndex].Cells["procstat"].Value?.ToString();
+                    string currentDeliStat = dataGridView.Rows[e.RowIndex].Cells["delistat"].Value?.ToString();
+
+                    // "완료"에서 다른 값으로 변경하면 날짜를 다시 초기화하고 값을 현재 셀의 데이터로 만듭니다.
+                    if ((currentProcStat != null && currentProcStat != "완료") || (currentDeliStat != null && currentDeliStat != "완료"))
                     {
-                        dataGridView.Rows[e.RowIndex].Cells["delidate"].Value = DateTime.Now.ToString("yyyy-MM-dd");
+                        dataGridView.Rows[e.RowIndex].Cells["delidate"].Value = DBNull.Value;
+                        dataGridView.Rows[e.RowIndex].Cells["procstat"].Value = currentProcStat;
+                        dataGridView.Rows[e.RowIndex].Cells["delistat"].Value = currentDeliStat;
+
+                        string salecode = dataGridView.Rows[e.RowIndex].Cells["salecode"].Value.ToString();
+
+                        using (MySqlConnection iConn = new MySqlConnection(jConn))
+                        {
+                            iConn.Open();
+                            MySqlCommand msc = new MySqlCommand("UPDATE sale2 SET procstat = @procstat, delistat = @delistat, delidate = @delidateValue WHERE salecode = @salecode", iConn);
+                            msc.Parameters.AddWithValue("@procstat", currentProcStat);
+                            msc.Parameters.AddWithValue("@delistat", currentDeliStat);
+                            msc.Parameters.AddWithValue("@delidateValue", DBNull.Value); // 날짜를 초기화
+                            msc.Parameters.AddWithValue("@salecode", salecode);
+
+                            msc.ExecuteNonQuery();
+                        }
                     }
                     else
                     {
-                        dataGridView.Rows[e.RowIndex].Cells["delidate"].Value = DBNull.Value;
+                        // 변경된 값을 데이터베이스에 업데이트합니다.
+                        string salecode = dataGridView.Rows[e.RowIndex].Cells["salecode"].Value.ToString();
+
+                        using (MySqlConnection iConn = new MySqlConnection(jConn))
+                        {
+                            iConn.Open();
+                            MySqlCommand msc = new MySqlCommand("UPDATE sale2 SET procstat = @procstat, delistat = @delistat, delidate = @delidateValue WHERE salecode = @salecode", iConn);
+                            msc.Parameters.AddWithValue("@procstat", currentProcStat);
+                            msc.Parameters.AddWithValue("@delistat", currentDeliStat);
+                            msc.Parameters.AddWithValue("@delidateValue", DateTime.Now.ToString("yyyy-MM-dd"));
+                            msc.Parameters.AddWithValue("@salecode", salecode);
+
+                            msc.ExecuteNonQuery();
+                        }
                     }
+
+                    // 이전 값을 현재 값으로 업데이트합니다.
+                    dataGridView.Rows[e.RowIndex].Cells["procstat"].Tag = currentProcStat;
+                    dataGridView.Rows[e.RowIndex].Cells["delistat"].Tag = currentDeliStat;
                 }
             }
         }
-        
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+                string selectedSaleCode = selectedRow.Cells["salecode"].Value.ToString();
+
+                // DataGridView2에 해당 판매번호에 대한 세부 정보를 표시하는 함수 호출
+                LoadSaleDetails(selectedSaleCode);
+            }
+        }
+
+        private void LoadSaleDetails(string test)
+        {
+            dataGridView2.Visible = false;
+            MessageBox.Show("ok");
+        }
+
     }
 }
