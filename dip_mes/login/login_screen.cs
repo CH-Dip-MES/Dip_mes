d59b98c1db5bbb682e7bbb4fca361118460f77c2
건우 박 <pgw1729@gmail.com>
2023-12-22 15:26:28 +0900

    로그인화면 초안

--- dip_mes/login/login_screen.cs
+++ dip_mes/login/login_screen.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace dip_mes
 {
     public partial class login_screen : Form
     {
+        string jConn = "Server=222.108.180.36;Database=mes_2;Uid=EDU_STUDENT;Pwd=1234;";
         public login_screen()
         {
             InitializeComponent();
@@ -21,15 +23,49 @@ namespace dip_mes
         {
             string userID = ID.Text;
             string userPW = PW.Text;
+            if (string.IsNullOrEmpty(userID) || string.IsNullOrEmpty(userPW))
+            {
+                MessageBox.Show("ID/PW를 입력해주세요");
+                return;
+            }
+            using (MySqlConnection conn = new MySqlConnection(jConn))
+            {
+                conn.Open();
+                string log = "SELECT COUNT(*) FROM test WHERE id = @userID AND pwd = @userPW";
+                MySqlCommand cmd = new MySqlCommand(log, conn);
+                cmd.Parameters.AddWithValue("@userID", userID);
+                cmd.Parameters.AddWithValue("@userPW", userPW);
 
-            if (userID.Equals("admin") && userPW.Equals("1234"))
-            {
-                MessageBox.Show("로그인에 성공했습니다.", "로그인");
+                int result = Convert.ToInt32(cmd.ExecuteScalar());
+                if (result > 0)
+                {
+                    // MainScreen 인스턴스를 생성합니다.
+                    main_screen mainScreen = new main_screen();
+
+                    // 현재 로그인 폼을 숨깁니다. (또는 this.Close();를 사용해 닫을 수도 있습니다.)
+                    this.Hide();
+
+                    // MainScreen 폼을 표시합니다.
+                    mainScreen.ShowDialog();
+
+                    // MainScreen 폼이 닫히면 로그인 폼을 다시 표시합니다.
+                    this.Show();
+                }
+                else
+                {
+                    MessageBox.Show("해당하는 회원정보가 없습니다");
+                }
             }
-            else
-            {
-                MessageBox.Show("로그인에 실패했습니다", "로그인");
-            }
+        }
+        private void Find_Click(object sender, EventArgs e)
+        {
+            find_screen find_Screen = new find_screen();
+            find_Screen.Show();
+        }
+        private void New_Click(object sender, EventArgs e)
+        {
+            join_screen join_screen = new join_screen();
+            join_screen.Show();
         }
     }
 }
